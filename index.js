7f9b84032b2b6912f8218f95f2223cae95c3ca4d
Davison Pro <33730223+Davisonpro@users.noreply.github.com>
2019-01-13 14:09:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -42,8 +42,24 @@ console.log(
 
 // Start a development HTTPS server.
 if ( protocol === 'https' ) {
+	const { execSync } = require( 'child_process' );
+	const execOptions = { encoding: 'utf-8', windowsHide: true };
 	let key = './certs/key.pem';
-    let certificate = './certs/certificate.pem';
+	let certificate = './certs/certificate.pem';
+	
+	if ( ! fs.existsSync( key ) || ! fs.existsSync( certificate ) ) {
+		try {
+			execSync( 'openssl version', execOptions );
+			execSync(
+				`openssl req -x509 -newkey rsa:2048 -keyout ./certs/key.tmp.pem -out ${ certificate } -days 365 -nodes -subj "/C=US/ST=Foo/L=Bar/O=Baz/CN=localhost"`,
+				execOptions
+			);
+			execSync( `openssl rsa -in ./certs/key.tmp.pem -out ${ key }`, execOptions );
+			execSync( 'rm ./certs/key.tmp.pem', execOptions );
+		} catch ( error ) {
+			console.error( error );
+		}
+	}
 
 	const options = {
 		key: fs.readFileSync( key ),
